40fd8dd47fc10e94b52fe2f3e05717c1e8fa2c75
Krishnalal P <krishnalal@qburst.com>
2021-06-24 13:01:38 +0530

    Update js file

--- kai.js
+++ kai.js
@@ -1,19 +1,4 @@
 (async function () {
-    let result;
-    result = await new Promise(function (resolve) {
-        https.request("https://www.jslint.com/jslint.js", function (res) {
-            result = "";
-            res.on("data", function (chunk) {
-                result += chunk;
-            }).on("end", function () {
-                resolve(result);
-            }).setEncoding("utf8");
-        }).end();
-    });
-    result = jslint(result);
-    result.warnings.forEach(function ({
-        formatted_message
-    }) {
-        console.error(formatted_message);
-    });
+  const age = 17;
+  console.log(`Age: ${age}`);
 }());
